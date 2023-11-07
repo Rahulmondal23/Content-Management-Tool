6ce2b7240973c820cf1c2fbd45277d83ff0b6921
Rahul Mondal <150150197+Rahulmondal23@users.noreply.github.com>
2023-11-07 21:46:26 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+<footer id="footer">
+          <div id="slideContainer">
+            <div id="slide1">
+              <div id="desc">
+                <div id="title">CMS (Content Management System)</div>
+                <div id="details">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                  voluptates, suscipit cupiditate temporibus.
+                </div>
+              </div>
+              <div id="socialBtnContainer">
+               
+                <a href="https://www.linkedin.com/in/rahul-mondal-772239228"><i class="fa-brands fa-linkedin"></i></a>
+              </div>
+              <div id="feedbackBtnContainer">
+                <button type="submit" id="feedbackBtn">Share your feedback!</button>
+              </div>
+            </div>
+    
+            <div id="slide2">
+              <div id="feedbackFormContainer">
+                <button id="feedbackBackBtn">
+                  <i class="fa-solid fa-backward"></i>
+                </button>
+                <div id="feedback">
+                  <div id="title">Write your Feedback!</div>
+                  <textarea
+                    id="feedbackContent"
+                    placeholder="Write here..."
+                  ></textarea>
+                  <button id="shareFeedback" type="submit">Share</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="developed">developed by Rahul Mondal </div>
+        </footer>
